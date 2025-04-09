56e58c940bc4b7261070e5664573197671040cb6
Dmitry <dimkin3@gmail.com>
2025-04-09 16:16:42 +0100

    Обновлены модели и маршруты для управления валютами и обменными курсами, добавлены функции для конвертации валюты и получения деталей валюты по коду.

--- model.py
+++ model.py
@@ -9,28 +9,40 @@ class CurrencyModel:
         conn = sqlite3.connect(self.db_path)
         cursor = conn.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS currencies (
-                            code TEXT PRIMARY KEY,
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            code TEXT NOT NULL UNIQUE,
                             name TEXT NOT NULL,
                             sign TEXT NOT NULL)''')
         cursor.execute('''CREATE TABLE IF NOT EXISTS exchange_rates (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
                             from_currency TEXT,
                             to_currency TEXT,
                             rate REAL,
-                            PRIMARY KEY (from_currency, to_currency),
-                            FOREIGN KEY (from_currency) REFERENCES currencies (code),
-                            FOREIGN KEY (to_currency) REFERENCES currencies (code))''')
+                            FOREIGN KEY (from_currency) REFERENCES currencies (id),
+                            FOREIGN KEY (to_currency) REFERENCES currencies (id))''')
         conn.commit()
         conn.close()
+
+    def add_currency(self, code, name, sign):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        try:
+            cursor.execute('INSERT INTO currencies (code, name, sign) VALUES (?, ?, ?)', (code, *currency_sign.get(code, (None, None))))
+            conn.commit()
+        except sqlite3.IntegrityError:
+            raise ValueError("Currency already exists")
+        finally:
+            conn.close()
 
     def get_currency_by_code(self, code):
         conn = sqlite3.connect(self.db_path)
         cursor = conn.cursor()
         print(f"Fetching currency with code: {code}")
-        cursor.execute('SELECT code, name, sign FROM currencies WHERE code = ?', (code,))
+        cursor.execute('SELECT id, code, name, sign FROM currencies WHERE code = ?', (code,))
         result = cursor.fetchone()
         conn.close()
         if result:
-            return {'code': result[0], 'name': result[1], 'sign': result[2] }
+            return {'id' : result[0], 'code': result[1], 'name': result[2], 'sign': result[3]  }
         else:
             return None
     
@@ -45,17 +57,22 @@ class CurrencyModel:
         
         conn.close()
         return currencies
-
-    def add_currency(self, code, name):
+    
+    def get_exchange_rate(self, to_currency, from_currency):
         conn = sqlite3.connect(self.db_path)
         cursor = conn.cursor()
-        try:
-            cursor.execute('INSERT INTO currencies (code, name, sign) VALUES (?, ?, ?)', (code, *currency_sign.get(code, (None, None))))
-            conn.commit()
-        except sqlite3.IntegrityError:
-            raise ValueError("Currency already exists")
-        finally:
-            conn.close()
+        cursor.execute('SELECT id, from_currency, to_currency, rate FROM exchange_rates WHERE from_currency = ? AND to_currency = ?',
+                    (from_currency, to_currency))
+        result = cursor.fetchone()
+        conn.close()
+        print(to_currency, from_currency)
+        print(f"Result: {result}")
+        if result:
+            return {'id' : result[0], 'to_currency': result[1], 'from_currency': result[2], 'rate': result[3] }
+        else:
+            return None
+            #значит валютная пара не найдена
+
 
     def get_exchange_rates(self):
         conn = sqlite3.connect(self.db_path)
@@ -64,20 +81,31 @@ class CurrencyModel:
         columns = [description[0] for description in cursor.description]
         rows = cursor.fetchall()
         rates = [dict(zip(columns, row)) for row in rows]
+        result = cursor.fetchone()
         conn.close()
+
         return rates
+
 
     def add_exchange_rate(self, from_currency, to_currency, rate):
         conn = sqlite3.connect(self.db_path)
         cursor = conn.cursor()
+
         try:
+            # Проверяем, существует ли уже курс обмена
+            cursor.execute('SELECT 1 FROM exchange_rates WHERE from_currency = ? AND to_currency = ?',
+                           (from_currency, to_currency))
+            if cursor.fetchone():
+                raise ValueError("Exchange rate already exists")
+
+            # Если курса нет, добавляем его
             cursor.execute('INSERT INTO exchange_rates (from_currency, to_currency, rate) VALUES (?, ?, ?)',
                            (from_currency, to_currency, rate))
             conn.commit()
-        except sqlite3.IntegrityError:
-            raise ValueError("Exchange rate already exists")
+            return rate
         finally:
             conn.close()
+        
 
     def convert_currency(self, from_currency, to_currency, amount):
         conn = sqlite3.connect(self.db_path)
@@ -91,3 +119,4 @@ class CurrencyModel:
             return amount * rate
         else:
             raise ValueError("Exchange rate not found")
+        
